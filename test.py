dbece533e8259ebc2652a586a53948e7f858508f
Dmitry Abramov <dabramov@aarcapital.com>
2019-08-15 23:26:11 +0300

    Тест новой фичи

--- test.py
+++ test.py
@@ -3,10 +3,11 @@ import unittest
 
 class TestStudy(unittest.TestCase):
     def test_1(self):
-        self.assertEqual(2, 1 + 1)
+        self.assertEqual(6, 1 + 2 + 3)
+        self.assertEqual(6, 1 * 2 * 3)
 
     def test_2(self):
-        self.assertTrue('hello')
+        self.assertTrue('This is a new feature')
 
 
 if __name__ == '__main__':
